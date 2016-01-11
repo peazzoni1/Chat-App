8f61a920248475772c90567e747292c5a437135a
peazzoni1 <peazzoni@yahoo.com>
2016-01-11 13:40:15 -0500

    change port 3000 to 80

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ socket.on('add user', function (username) {
     });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+  console.log('listening on *:80');
 });
